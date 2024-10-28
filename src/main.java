3c34ae13be4d837ae50080f1810a814257833e3f
EnricoSpanier <149034926+EnricoSpanier@users.noreply.github.com>
2024-10-28 20:36:16 -0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,36 +1,41 @@
-// Source code is decompiled from a .class file using FernFlower decompiler.
 import java.util.Scanner;
-
 public class main {
-   public main() {
-   }
+	public static void main(String args[]){
+		AVLTree arvore = new AVLTree();
 
-   public static void main(String[] var0) {
-      new AVLTree();
-      Scanner var2 = new Scanner(System.in);
-      short var5 = 1;
+		Scanner sc = new Scanner(System.in);
 
-      while(var5 != 1350) {
-         System.out.println("1- inserir valor\n2- mostrar arvore\n5-sair\n");
-         int var4 = var2.nextInt();
-         switch (var4) {
-            case 1:
-               int var3 = var2.nextInt();
-               System.out.println("ok" + var3);
-               break;
-            case 2:
-               System.out.println("arvore");
-               break;
-            case 3:
-            case 4:
-            default:
-               System.out.println("erro");
-               break;
-            case 5:
-               var5 = 1350;
-               var2.close();
-         }
-      }
+		int aux;
+		int op;
+		int po2=1;		
+		String teste = sc.nextLine();
+		System.err.println("foi :" + teste);
+		while(po2!=1350){
+			System.out.println("1- inserir valor\n2- mostrar arvore\n5-sair\n");
+			op = sc.nextInt();
+			switch(op){
+				case 1:
+					aux = sc.nextInt();
+					//arvore.insert(aux);
+					System.out.println("ok"+aux);
+					break;
+				
+				case 2:
+					//arvore.preOrderTraversal();
+					System.out.println("arvore");
+					break;
 
-   }
+				case 5:
+					po2=1350;
+					sc.close();
+					break;
+					
+				default:
+				    System.out.println("erro");
+
+			}
+
+		}
+		
+	}
 }
