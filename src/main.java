44fb2dd1b8685d265edc09561e0d2526b32bad32
EnricoSpanier <149034926+EnricoSpanier@users.noreply.github.com>
2024-10-28 20:42:38 -0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -8,23 +8,26 @@ public class main {
 		int aux;
 		int op;
 		int po2=1;		
-		String teste = sc.nextLine();
-		System.err.println("foi :" + teste);
 		while(po2!=1350){
 			System.out.println("1- inserir valor\n2- mostrar arvore\n5-sair\n");
 			op = sc.nextInt();
 			switch(op){
 				case 1:
 					aux = sc.nextInt();
-					//arvore.insert(aux);
-					System.out.println("ok"+aux);
+					arvore.insert(aux);
+					System.out.println("valor "+aux+" inserido");
 					break;
 				
 				case 2:
-					//arvore.preOrderTraversal();
+					arvore.preOrderTraversal();
 					System.out.println("arvore");
 					break;
 
+				case 3:
+					Syste.out.println("apagada");
+					break;
+
+					
 				case 5:
 					po2=1350;
 					sc.close();
