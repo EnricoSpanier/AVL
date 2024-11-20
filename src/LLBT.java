24eaaf0e7181b9ecfdbbd1b17eadbcb165f056c3
endydragonn <72773527+endydragonn@users.noreply.github.com>
2024-11-20 19:32:42 -0300

    Update LLBT.java

--- src/LLBT.java
+++ src/LLBT.java
@@ -1,30 +1,33 @@
 public class LLBT {
     protected BinaryTree bt;
     protected BinaryTree next;
-    
-    public void LLBT() {
-		this(null);
-	}
 
-	public void LLBT(BinaryTree bt) {
-		this.bt = bt;
-	}
-	public void LLBT(BinaryTree bt, BinaryTree next){
-	    this.bt = bt;
-	    this.next = next;
-	}
-	
-		public BinaryTree getBt(){
-	    return bt;
-	}
-	public void setBT(BinaryTree bt){
-	    this.bt = bt;
-	}
-	
-	public BinaryTree getNext(){
-	    return next;
-	}
-	public void setNext(BinaryTree next){
-	    this.next = next;
-	}
+    public LLBT() {
+        this(null, null);
+    }
+
+    public LLBT(BinaryTree bt) {
+        this.bt = bt;
+    }
+
+    public LLBT(BinaryTree bt, BinaryTree next) {
+        this.bt = bt;
+        this.next = next;
+    }
+
+    public BinaryTree getBT() {
+        return bt;
+    }
+
+    public void setBT(BinaryTree bt) {
+        this.bt = bt;
+    }
+
+    public BinaryTree getNext() {
+        return next;
+    }
+
+    public void setNext(BinaryTree next) {
+        this.next = next;
+    }
 }
